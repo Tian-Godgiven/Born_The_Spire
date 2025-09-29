6be33678a5215e980b492fd160fd37f78d58758c
Tian-Godgiven <tiannotsky@gmail.com>
2025-09-29 15:15:12 +0800

    关键触发器和默认触发器

--- born_the_spire/src/objects/system/trigger/defaultTrigger.ts
+++ born_the_spire/src/objects/system/trigger/defaultTrigger.ts
@@ -1,7 +1,20 @@
 //默认触发器相关
+import { TriggerObj } from "@/types/object/trigger";
+import { Trigger } from "./Trigger";
 
-import { Entity } from "../Entity";
+export type DefaultTrigger = {
+    triggerWay:string,//例如 after_via_useCard
+    id:string,
+    remove:()=>void,
+    info?:string
+}
 
-function getDefaultTrigger(target:Entity){
-
+export function getDefaultTrigger(triggerManager:Trigger,triggerObj:TriggerObj,id:string,remove:()=>void,info?:string){
+    const defaultTrigger:DefaultTrigger = {
+        triggerWay:`${triggerObj.when}_${triggerObj.how}_${triggerObj.key}`,
+        id,
+        remove,
+        info
+    } 
+    triggerManager._defaultTrigger.push(defaultTrigger)
 }
