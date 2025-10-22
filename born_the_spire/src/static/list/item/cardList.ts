b9155adf60bfa3d89882c9abf7c0ede6c3d7e1f2
Tian-Godgiven <tiannotsky@gmail.com>
2025-10-22 17:55:53 +0800

    物品和交互

--- born_the_spire/src/static/list/item/cardList.ts
+++ born_the_spire/src/static/list/item/cardList.ts
@@ -1,17 +1,12 @@
 
 import { Card } from "@/objects/item/Card"
 import { ItemMap } from "@/objects/item/Item"
-import { EffectUnit } from "@/objects/system/effect/EffectUnit"
 
 export type CardMap = ItemMap & {
     status:{
         cost:number|null
     }
     entry?:string[],
-    useCard:{
-        target:"enemy"|"player"
-        effect:EffectUnit[]//使用卡牌将会造成的效果
-    }
 }
 
 const cardList:CardMap[] = [{
@@ -21,18 +16,18 @@ const cardList:CardMap[] = [{
         cost:1,
     },
     describe:[
-        "造成",
-        {
-            key:["status","damage"]
-        },
-        "点伤害"
+        "造成",{key:["status","damage"]},"点伤害"
     ],
     key:"original_card_00001",
-    useCard:[{
-        key:"damage",
-        params:{value:5}
-        targetType:"enemy"
-    }]
+    interaction:{
+        use:{
+            target:{faction:"enemy"},
+            effects:[{
+                key:"damage",
+                params:{value:5},
+            }]
+        }
+    }
 },{
     label:"消耗打击",
     status:{
@@ -41,18 +36,18 @@ const cardList:CardMap[] = [{
     },
     entry:["exhaust"],
     describe:[
-        "造成",
-        {
-            key:["status","damage"]
-        },
-        "点伤害"
+        "造成",{key:["status","damage"]},"点伤害"
     ],
     key:"original_card_00002",
-    useCard:[{
-        key:"damage",
-        value:5,
-        targetType:"enemy"
-    }]
+    interaction:{
+        use:{
+            target:{faction:"enemy"},
+            effects:[{
+                key:"damage",
+                params:{value:5},
+            }]
+        }
+    }
 },{
     label:"虚无打击",
     status:{
@@ -61,19 +56,17 @@ const cardList:CardMap[] = [{
     },
     entry:["void"],
     describe:[
-        "造成",
-        {
-            key:["status","damage"]
-        },
-        "点伤害"
+        "造成",{key:["status","damage"]},"点伤害"
     ],
     key:"original_card_00003",
-    behavior:{
-        useCard:[{
-            key:"damage",
-            value:5,
-            targetType:"enemy"
-        }]
+    interaction:{
+        use:{
+            target:{faction:"enemy"},
+            effects:[{
+                key:"damage",
+                params:{value:5},
+            }]
+        }
     }
 },{
     label:"肌肉强化",
@@ -82,26 +75,16 @@ const cardList:CardMap[] = [{
         cost:0
     },
     entry:["exhaust"],
-    describe:[
-        "获得",
-        {
-            key:["status","power"]
-        },
-        "层力量"
-    ],
+    describe:["获得",{key:["status","power"]},"层力量"],
     key:"original_card_00004",
-    behavior:{
-        useCard:[{
-            key:"getState",
-            info:{
-                state:{
-                    key:"power",
-                    stacks:1
-                }
-            },
-            value:0,
-            targetType:"self"
-        }]
+    interaction:{
+        use:{
+            target:{faction:"player",key:"self"},
+            effects:[{
+                key:"getStatus",
+                params:{statusKey:"power",stacks:1},
+            }]
+        }
     }
 }]
 
